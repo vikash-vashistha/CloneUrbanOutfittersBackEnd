c66251b1ccbf31b8e6e1d821344630a65d8f73ce
skale0181 <skale0181@gmail.com>
2022-02-23 10:42:44 +0530

    change collection

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,6 +1,12 @@
+// const mongoose = require("mongoose");
+// module.exports = () => {
+//   return mongoose.connect(
+//     "mongodb+srv://skale0181:sagar@@cluster0.wqpjq.mongodb.net/Urben-Outfitters?retryWrites=true&w=majority"
+//   );
+// }
 const mongoose = require("mongoose");
 module.exports = () => {
   return mongoose.connect(
-    "mongodb+srv://skale0181:sagar@@cluster0.wqpjq.mongodb.net/Urben-Outfitters?retryWrites=true&w=majority"
+    "mongodb+srv://skale0181:sagar@cluster0.wqpjq.mongodb.net/products_data"
   );
 }
