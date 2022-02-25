7c3925384a5e25233321028b0d257e87f9261c1c
skale0181 <skale0181@gmail.com>
2022-02-25 13:36:40 +0530

    product pages conect to the backend

--- src/index.js
+++ src/index.js
@@ -6,11 +6,28 @@ const bagController = require("./controllers/bag.controller");
 const { register, login } = require("./controllers/auth.controller");
 const { body } = require("express-validator");
 
+//----------------product page controllers-----------------//
+const womenController = require("./controllers/womenController")
+const womenshoeController = require("./controllers/womenshoeController")
+const menController = require("./controllers/menController")
+const homeController = require("./controllers/homeController")
+const lifestyleController = require("./controllers/lifestyleController")
+//-------------------//------------------------------//----------//
+
 const app = express();
 
 app.use(express.json());
 app.use("/users", userController);
 app.use("/bag", bagController);
+
+//----------------product page use controllers------------------//
+app.use("/womens", womenController);
+app.use("/womenshoes", womenshoeController);
+app.use("/mens", menController);
+app.use("/homes", homeController);
+app.use("/lifestyles", lifestyleController);
+
+//-------------------//---------------------------//---------------//
 
 app.post("/register", body("email").notEmpty(), register);
 app.post("/login", login);
